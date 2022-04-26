695702ab856972cad78c0fcfd85c094d4f948b05
apidev02 <apidev02>
2022-04-26 17:01:00 +0700

    update user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,6 +7,12 @@ const router = express.Router();
 router.route('/signup').post(authController.signUp);
 router.route('/login').post(authController.login);
 
+router.route('/forgotPassword').post(authController.forgotPassword);
+router.route('/resetPassword').patch(authController.resetPassword);
+router.route('/updateMyPassword').patch(authController.protect, authController.updatePassword);
+router.route('/updateMe').patch(authController.protect, authController.updateMe);
+router.route('/deleteMe').delete(authController.protect, authController.deleteMe);
+
 router
   .route('/')
   .get(userController.getAllUser)
